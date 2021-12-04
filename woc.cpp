16ab3af9e91179cec9538226746bc978cdd2dded
ScientistYT <89310370+ScientistYT@users.noreply.github.com>
2021-12-04 12:52:51 +0300

    Update woc.cpp

--- woc.cpp
+++ woc.cpp
@@ -3,15 +3,24 @@ void wheels();
 void caterpillar();
 int main()
 {
+
+    int x = 635;
+	int y = 454;
+
 	txCreateWindow(1500,700);
 	txSetFillColor(RGB(0,255,255));
 	txRectangle(0,0,1500,700);
 
 	txSetColor(TX_BLACK,5);
 	txSetFillColor(TX_BROWN);
-	txRectangle(530,200,750,300);
-	txRectangle(428,300,873,414);
-	POINT gun[4] = { {751, 230}, {1006, 230}, {1006, 248}, {751, 248} };
+	txRectangle(x - 635 + 530,y - 454 + 200,x - 635 + 750,y - 454 + 300);
+	txRectangle(x - 635 + 428,y - 454 + 300,x - 635 + 873,y - 454 + 414);
+	POINT gun[4] = {
+	{x - 635 + 751, y - 454 + 230},
+	{x - 635 + 1006,y - 454 + 230},
+	{x - 635 + 1006, y - 454 + 248},
+	{x - 635 + 751, y - 454 + 248}
+	};
 	txPolygon(gun, 4);
 
 	wheels();
@@ -51,7 +60,11 @@ void caterpillar()
 	int y = 454;
 	int r = 33;
 
-	POINT caterpillar[5] = {{x - (40 + (r - 23)), y - 35}, {x + ((150 * 2) + (r * 2)) + 27, y - 35}, {x + ((r * 9) + 80), y + 35}, {x + (r - 10), y + 35}, {x - r, y + 35} };
+	POINT caterpillar[4] =
+	{ {x - (40 + (r - 23)), y - 35},
+    {x + ((150 * 2) + (r * 2)) + 27, y - 35},
+    {x + ((r * 9) + 73), y + 35},
+    {x - (r - 5), y + 35} };
 
-	txPolygon(caterpillar, 5);
+	txPolygon(caterpillar, 4);
 }
