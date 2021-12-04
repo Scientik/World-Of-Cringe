bde0e58e4dda2928835b552d0f52c5a3490da53f
ScientistYT <89310370+ScientistYT@users.noreply.github.com>
2021-12-04 20:34:20 +0300

    Update woc.cpp

--- woc.cpp
+++ woc.cpp
@@ -7,18 +7,18 @@ int r = 33;
 int main()
 {
 
-    int x = 350;
-    int y = 454;
-    int r = 33;
+	int x = 350;
+	int y = 454;
+	int r = 33;
 
-	txCreateWindow(1500,700);
-	txSetFillColor(RGB(255,255,255));
-	txRectangle(0,0,1500,700);
+	txCreateWindow(1500, 700);
+	txSetFillColor(RGB(255, 255, 255));
+	txRectangle(0, 0, 1500, 700);
 
-	txSetColor(TX_BLACK,5);
+	txSetColor(TX_BLACK, 5);
 	txSetFillColor(TX_BROWN);
-	txRectangle(x - 635 + 530,y - 454 + 200,x - 635 + 750,y - 454 + 300);
-	txRectangle(x - 635 + 428,y - 454 + 300,x - 635 + 873,y - 454 + 414);
+	txRectangle(x - 635 + 530, y - 454 + 200, x - 635 + 750, y - 454 + 300);
+	txRectangle(x - 635 + 428, y - 454 + 300, x - 635 + 873, y - 454 + 414);
 	POINT gun[4] = {
 	{x - 635 + 751, y - 454 + 230},
 	{x - 635 + 1006,y - 454 + 230},
@@ -44,40 +44,41 @@ int main()
 
 void wheels()
 {
-	x = x-157;
+	x = x - 157;
 	y = 454;
 	r = 33;
-	txCircle(x,y,r);
+	txCircle(x, y, r);
 
-	 int number = 0;
+	int xwheels = x;
+	int ywheels = y;
+
+	int number = 0;
 
 	while (number < 4)
 	{
-		x = x + ((r * 2) + 20);
-		txCircle(x, y, r);
-		number ++;
+		xwheels = xwheels + ((r * 2) + 20);
+		txCircle(xwheels, ywheels, r);
+		number++;
 	}
-
-	x = 635-157;
 
 	txCircle(x - 40, y - 25, r - 25);
 	txCircle(x + 42.5, y - 25, r - 25);
-	txCircle(x + (150*2), y - 25, r - 25);
-	txCircle(x + ((150*2) + (r*2)) + 17.5, y - 25, r - 25);
+	txCircle(x + (150 * 2), y - 25, r - 25);
+	txCircle(x + ((150 * 2) + (r * 2)) + 17.5, y - 25, r - 25);
 }
 
 void caterpillar()
 {
 	txSetFillColor(TX_TRANSPARENT);
-	x = x - 157;
+	x = 350 - 157;
 	y = 454;
 	r = 33;
 
 	POINT caterpillar[4] =
 	{ {x - (40 + (r - 23)), y - 35},
-    {x + ((150 * 2) + (r * 2)) + 27, y - 35},
-    {x + ((r * 9) + 73), y + 35},
-    {x - (r - 5), y + 35} };
+	{x + ((150 * 2) + (r * 2)) + 27, y - 35},
+	{x + ((r * 9) + 73), y + 35},
+	{x - (r - 5), y + 35} };
 
 	txPolygon(caterpillar, 4);
 }
